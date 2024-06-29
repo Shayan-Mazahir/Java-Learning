e064ce1adb46db8c9afbb553a9a6a8d74366cbe0
Ruz <shayanmazahir321@gmail.com>
2024-06-28 21:32:26 -0400

    Added the notes and chapter 0

---
+++ programs/Chapter 0/Main.java
@@ -0,0 +1,8 @@
+package com.company; // Groups classes
+
+public class Main {	
+    // Entrypoint into the application
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

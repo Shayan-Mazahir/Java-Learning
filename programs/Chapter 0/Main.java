ccc5ec7df6b33a0dd72544a498ff1d9356dd1160
Ruz <shayanmazahir321@gmail.com>
2024-06-28 21:40:16 -0400

    Added my first program

--- programs/Chapter 0/Main.java
+++ programs/Chapter 0/Main.java
@@ -1,8 +1,6 @@
 package com.company; // Groups classes
-
-public class Main {	
-    // Entrypoint into the application
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+public class Main{	// Entrypoint into the application
+	public static void main(String[]args){
+		System.out.println("Hello World");
+	}
 }

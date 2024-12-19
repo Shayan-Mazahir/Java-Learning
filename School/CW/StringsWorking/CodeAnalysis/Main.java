770d8b682b61046701af90617bf38c5e2edeb157
Ruz <shayanmazahir321@gmail.com>
2024-12-19 08:28:35 -0500

    finished code analysis

--- School/CW/StringsWorking/CodeAnalysis/Main.java
+++ School/CW/StringsWorking/CodeAnalysis/Main.java
@@ -24,7 +24,39 @@ public class Main {
 
             //Question 5 
             System.out.println(string3.lastIndexOf("ub"));
-            //
+            //no idea
+
+            //Question 6
+            System.out.println(string3.charAt(4));
+            //HubbaBubba - a
+
+            //Question 7
+            System.out.println(string2.substring(1));
+            //no idea
+
+            //Question 8
+            System.out.println(string3.substring(3,6));
+            //no idea
+
+            //Question 9
+            System.out.println(string1.equals("HUBBA"));
+            //false
+
+            //Question 10
+            System.out.println(string1.compareTo(string2));
+            //again, how do i compare UNICODES?
+
+            //Question 11
+            System.out.println(string1.compareTo(string1));
+            //again... unicodes?
+
+            //Question 12
+            System.out.println(string1.compareTo("HUBBA"));
+            //UNICODES?!
+
+            //Question 13
+            System.out.println(string1.compareTo("Habba"));
+            //UNICODES?
             
 
     }
